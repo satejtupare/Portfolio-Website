c530ac67059dd618a769507684ddaad5beb01443
satej tupare <satejtupare@gmail.com>
2023-12-25 00:05:31 +0530

    the frontend is completed

---
+++ frontend/main.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  getVisitCount();
+});
+
+//const productionApiUrl = 'https://azureresumevisitorcounter.azurewebsites.net/api/GetVisitorCounter?';
+const localApiUrl = "http://localhost:7071/api/GetVisitorCounter";
+
+const getVisitCount = () => {
+  let count = 30;
+  fetch(localApiUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log("Website called function API.");
+      count = response.count;
+      document.getElementById("counter").innerText = count;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return count;
+};
